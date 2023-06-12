68bb7048dd4d31cf03e0a74267c9a5e2be8cb20f
Arkxd <74634345+Arkxd@users.noreply.github.com>
2023-06-12 15:33:38 +0800

    1

--- src/日期计算/Test.java
+++ src/日期计算/Test.java
@@ -62,7 +62,6 @@ public class Test {
             System.out.println("年份错误");
         }
     }
-
     public static boolean NumberExit(int[] array, int number) {
         for (int j : array) {
             if (number == j) {
